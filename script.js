c0c7068d5fb93bb9f0873523e940eb829af58e92
Evandro Brandão <evandrobrandao@hotmail.com>
2024-10-28 19:01:43 -0300

    Atualização do formulário II

--- script.js
+++ script.js
@@ -17,37 +17,72 @@ document
     document.getElementById("contact-form").reset();
   });
 
+  // ---------- FUNÇÔES GERAIS ---------- //
+  function togglePopup(input, label) {
+    // Mostrar popup de campo obrigatório
+    input.addEventListener("focus", ()=> {
+      label.classList.add("required-popup");
+    });
+  
+
+  // Ocultar popup de campo obrigatório
+  input.addEventListener("blur",() =>  {
+    label.classList.remove("required-popup");
+  })};
+
+  function estilizarInputCorreto(input, helper) {
+    helper.classList.remove("visible");
+    input.classList.remove("error");
+    input.classList.add("correct");
+
+  }
+
+  function estilizarInputIncorreto(input, helper) {
+    helper.classList.add("visible");
+    input.classList.remove("error");
+    input.classList.add("correct");
+  }  
+
 // ---------- VALIDAÇÃO USERNAME ---------- //
 let nameInput = document.getElementById("name");
 let nameLabel = document.querySelector('label[for="name"]');
 let nameHelper = document.getElementById("name-helper");
 
-function mostrarPopup(input, label) {
-  // Mostrar popup de campo obrigatório
-  nameInput.addEventListener("focus", () => {
-    nameLabel.classList.add("required-popup");
-  });
-}
+togglePopup(nameInput, nameLabel)
 
-// Ocultar popup de campo obrigatório
-nameInput.addEventListener("blur", () => {
-  nameLabel.classList.remove("required-popup");
-});
+// function mostrarPopup(input, label) {
+//   // Mostrar popup de campo obrigatório
+//   nameInput.addEventListener("focus", () => {
+//     nameLabel.classList.add("required-popup");
+//   });
+// }
 
-// Validar valor do input para o name
+// // Ocultar popup de campo obrigatório
+// nameInput.addEventListener("blur", () => {
+//   nameLabel.classList.remove("required-popup");
+// });
+
+// Validar valor do input para o username
 nameInput.addEventListener("change", (e) => {
-  let valor = e.target.value;
+  let valor = e.target.value
   // console.log(valor);
 
   if (valor.length < 3) {
-    nameInput.classList.remove("correct");
-    nameInput.classList.add("error");
-    nameHelper.innerText = "Necessário ao menos 3 caracteres";
-    nameHelper.classList.add("visible");
+    // Adicionar estilos dinâmicos se o valor tiver menos de 3 caracteres
+    nameHelper.innerText = "Seu username deve ter ao menos 3 ou mais caracteres";
+    estilizarInputIncorreto(nameInput, nameHelper)    
+    inputsCorretos.username = false
+    // nameInput.classList.remove("correct");
+    // nameInput.classList.add("error");
+    // nameHelper.innerText = "Necessário ao menos 3 caracteres";
+    // nameHelper.classList.add("visible");
   } else {
-    nameInput.classList.remove("error");
-    nameHelper.classList.remove("visible");
-    nameInput.classList.add("correct");
+    // Adicionar estilos dinâmicos se o valor estiver correto
+    estilizarInputCorreto(nameInput, nameHelper);
+    inputsCorretos.username = true
+    // nameInput.classList.remove("error");
+    // nameHelper.classList.remove("visible");
+    // nameInput.classList.add("correct");
   }
 });
 
@@ -56,32 +91,33 @@ let emailInput = document.getElementById("email");
 let emailLabel = document.querySelector('label[for="email"]');
 let emailHelper = document.getElementById("email-helper");
 
-// Mostrar popup de campo obrigatório para o email
+togglePopup(emailInput, emailLabel)
 
-emailInput.addEventListener("focus", () => {
-  emailLabel.classList.add("required-popup");
-});
+
+//   emailLabel.classList.add("required-popup");
+// });
 
 // Ocultar popup de campo obrigatório para o email
-emailInput.addEventListener("blur", () => {
-  emailLabel.classList.remove("required-popup");
-});
-mostrarPopup(emailInput, emailLabel);
+// emailInput.addEventListener("blur", () => {
+//   emailLabel.classList.remove("required-popup");
+// });
+// mostrarPopup(emailInput, emailLabel);
 
-// Criação de funções para mostrar acerto e erro do input email
-function mostrarAcerto(input, helper) {
-  input.classList.remove("error");
-  helper.classList.remove("visible");
-  input.classList.add("correct");
-}
+// // Criação de funções para mostrar acerto e erro do input email
+// function mostrarAcerto(input, helper) {
+//   input.classList.remove("error");
+//   helper.classList.remove("visible");
+//   input.classList.add("correct");
+// }
 
-function mostrarError(input, helper, menssagem) {
-  input.classList.remove("correct");
-  input.classList.add("error");
-  helper.innerText = menssagem;
-  helper.classList.add("visible");
-}
+// function mostrarError(input, helper, menssagem) {
+//   input.classList.remove("correct");
+//   input.classList.add("error");
+//   helper.innerText = menssagem;
+//   helper.classList.add("visible");
+// }
 
+// Validar valor do input
 emailInput.addEventListener("change", (e) => {
   let valor = e.target.value;
 
@@ -89,12 +125,78 @@ emailInput.addEventListener("change", (e) => {
     // emailInput.classList.remove("error");
     // emmailHelper.classList.remove("visible");
     // emailInput.classList.add("correct");
-    mostrarAcerto(emailInput, emailHelper);
+    estilizarInputCorreto(emailInput, emailHelper);
+    inputsCorretos.email = true
+    // mostrarAcerto(emailInput, emailHelper);
   } else {
     // emailInput.classList.remove("correct");
     // emailInput.classList.add("error");
     // emailHelper.innerText = "Entre com um email válido";
     // emailHelper.classList.add("visible");
-    mostrarError(emailInput, emailHelper, "Entre com um email válido");
+    // mostrarError(emailInput, emailHelper, "Entre com um email válido");
+    emailHelper.innerText ="Precisa inserir um email válido";
+    estilizarInputIncorreto(emailInput, emailHelper);
+    inputsCorretos.email = false
   }
 });
+
+// ---------- VALIDAÇÂO SENHA ---------- //
+let senhaInput = document.getElementById("senha");
+let senhaLabel = document.querySelector('label[for="senha"]');
+let senhaHelper = document.getElementById("senha-helper");
+
+togglePopup(senhaInput, senhaLabel)
+
+senhaInput.addEventListener("blur", (e)=> {
+  let valor = e.target.value
+  if(valor == ""){
+    senhaHelper.innerText = "O campo senha não pode estar vazio"
+    estilizarInputIncorreto(senhaInput, senhaHelper)
+    inputsCorretos.senha = false
+  }else{
+    estilizarInputCorreto(senhaInput, senhaHelper)
+    inputsCorretos.senha = true
+  }
+})
+
+// ---------- VALIDAÇÂO CONFIRMA SENHA ---------- //
+let confirmaSenhaInput = document.getElementById("confirma-senha");
+let confirmasenhaLabel = document.querySelector('label[for="confirma-senha"]');
+let confirmasenhaHelper = document.getElementById("confirma-senha-helper");
+
+togglePopup(confirmaSenhaInput, confirmasenhaLabel)
+
+confirmaSenhaInput.addEventListener("blur", (e)=> {
+  let valorConfirmaSenha = e.target.value
+
+  if(valorConfirmaSenha == senhaInput.value){
+    estilizarInputCorreto(confirmaSenhaInput, confirmasenhaHelper)
+    inputsCorretos.confirmaSenha = true
+  }else{
+    confirmasenhaHelper.innerText = "As senhas precisam ser iguais"
+    estilizarInputIncorreto(confirmaSenhaInput, confirmasenhaHelper)
+    inputsCorretos.confirmaSenha = false
+  }
+})
+
+
+// ---------- EVITAR ENVIO DO FORMULÁRIO ---------- //
+let btnSubmit = document.querySelector('button[type="submit"]')
+let inputsCorretos = {
+  username: false,
+  email: false,
+  senha: false,
+  confirmaSenha: false
+  }
+
+  btnSubmit.addEventListener("click", (e)=> {
+    if (inputsCorretos.username == false || 
+        inputsCorretos.email == false ||
+        inputsCorretos.senha == false ||
+        inputsCorretos.confirmaSenha == false ) {
+        e.preventDefault()
+        alert("Os campos obrigatórios precisam ser preenchidos corretamente")
+    }else{
+      alert("Formulário enviado com sucesso")
+    }
+  })
